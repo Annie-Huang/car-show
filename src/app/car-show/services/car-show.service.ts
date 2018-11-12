fe39a16e2bc73a88a937d495297c695804be6827
Annie Huang <ahuang@seek.com.au>
2018-11-12 20:56:14 +1100

    fixed require

--- src/app/car-show/services/car-show.service.ts
+++ src/app/car-show/services/car-show.service.ts
@@ -1,23 +1,25 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
-import sortBy from 'lodash/sortBy';
+import sortBy from 'lodash.sortby';
 
 import {Show} from './show';
 import {CarWithShowInfo} from './car-with-show-info';
+
+declare var require: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarShowService {
 
-  constructor(private htpp: HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
   getShows(): Observable<Show[]> {
     const shows: Show[] = require('../../../resources/fixtures/carShows.json');
     return of(shows);
-    // return this.htpp.get('http://eacodingtest.digital.energyaustralia.com.au/api/v1/cars');
+    // return this.http.get('http://eacodingtest.digital.energyaustralia.com.au/api/v1/cars');
   }
 
   generateCarWithShowInfoListFromShows(shows: Show[]): CarWithShowInfo[] {
